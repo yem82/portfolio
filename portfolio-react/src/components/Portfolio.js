f119b8c0d02496384744226cd5eced89b209e33c
Yemi <yemiadebayo60@gmail.com>
2019-08-17 18:38:56 +0100

    initial structure with React

---
+++ portfolio-react/src/components/Portfolio.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+
+class Portfolio extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+
+    };
+  }
+
+  componentDidMount() {
+    fetch('/express')
+
+  }
+
+render() {
+  return (
+    <div>
+
+    </div>
+  )
+ }
+};
+
+export default Portfolio;
