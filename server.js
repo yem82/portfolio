799481d085cbf2d0f228fc9f19f73b32d754d7f4
Yemi <yemiadebayo60@gmail.com>
2019-09-01 20:02:46 +0100

    amend server.js

--- server.js
+++ server.js
@@ -55,10 +55,10 @@ const PORT = process.env.PORT || 5000;
 
 const path = require('path')
 
-app.use(express.static(path.join(__dirname, 'portfolio-react/build')))
+app.use(express.static(path.join(__dirname, 'client/build')))
 
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname + '/portfolio-react/build/index.html'))
+  res.sendFile(path.join(__dirname + '/client/build/index.html'))
 })
 
 app.listen(PORT, () => {
