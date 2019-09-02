25587c14d2012862a7e38af54045f1f2205cbc1f
Yemi <yemiadebayo60@gmail.com>
2019-09-02 23:12:21 +0100

    remove ajax request

--- server.js
+++ server.js
@@ -64,15 +64,3 @@ app.get('*', (req, res) => {
 app.listen(PORT, () => {
   console.log(`auto baby on ${PORT}!`);
 });
-
-$.ajax({
-  url: 'yemiadebayo.herokuapp.com',
-  type: 'POST',
-  headers: {'Accept': 'application/json;'},
-  data: {
-  "subject": "subject",
-  "message": "some body text"
-  },
-  }).done(function (res) {
-    console.log(res); // it shows your email sent message.
-});
